d82e9d3eddcf01926d4d9504b1024bf87e0b39eb
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-04-03 17:10:31 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,47 +1,50 @@
 const inputs = document.querySelectorAll(".inputs");
 const arrInputs = Array.from(inputs);
-// const label = document.getElementsByTagName("label")
-// const arrLabel = Array.from(label)
-const label = document.getElementById("nome2");
+const input = document.getElementById("primeiroNome")
+const label = document.getElementsByTagName("label")
+const arrLabel = Array.from(label)
+const label2 = document.getElementById("nome2");
 // const styleLabel = window.getComputedStyle(label);
-console.log(arrInputs);
 
-for (var elemento of arrInputs) {
-  elemento.addEventListener("click", function () {
-    for (i of arrInputs) {
-      i.classList.remove("focado");
+for (let e of arrInputs) {
+  e.addEventListener("focus",() => {
+    label2.style.transform = "translateY(-42px)"
+  })
+  e.addEventListener("blur", () => {
+    if (!input.value) {
+      label2.style.transform = "translateY(-25px)"
     }
-    this.classList.add("focado");
-  });
+  })
 }
 
-document.addEventListener("click", function (event) {
-  if (!arrInputs.includes(event.target)) {
-    for (let i = 0; i < arrInputs.length; i++) {
-      let campos = arrInputs[i];
-      if (campos.classList.contains("focado")) {
-        campos.classList.remove("focado");
-      }
-    }
-  }
-  for (i of arrInputs) {
-    if (i.classList.contains("focado")) {
-      label.style.transform = "translateY(-42px)"
-    } else {
-     
-    }
-  }
-});
+// for (var elemento of arrInputs) {
+//   elemento.addEventListener("click", function () {
+//     for (i of arrInputs) {
+//       i.classList.remove("focado");
+//     }
+//     this.classList.add("focado");
+//   });
+// }
 
+// document.addEventListener("click", function (event) {
+//   if (!arrInputs.includes(event.target)) {
+//     for (let i = 0; i < arrInputs.length; i++) {
+//       let campos = arrInputs[i];
+//       if (campos.classList.contains("focado")) {
+//         campos.classList.remove("focado");
+//       }
+//     }
+//   }
+// });
 
 // document.addEventListener("keydown", function () {
 //   for (i of arrInputs) {
 //     if (i.classList.contains("focado")) {
-//       i.style.height = "35px";
+//       label.style.transform = "translateY(-42px)";
 //     } else if (!i.classList.contains("focado") && i.value.length > 0) {
-//       i.style.height = "35px";
+//       label.style.transform = "translateY(-42px)";
 //     } else {
-//       i.style.height = "";
+//       label.style.transform = "translateY(-25px)";
 //     }
 //   }
 // });
