4bb5868cd747b06f191d9c733f2a2c5d84f984d1
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-04-03 00:08:52 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const inputs = document.querySelectorAll(".inputs");
 const arrInputs = Array.from(inputs);
+// const label = document.getElementsByTagName("label")
+// const arrLabel = Array.from(label)
+const label = document.getElementById("nome2");
+// const styleLabel = window.getComputedStyle(label);
 console.log(arrInputs);
 
 for (var elemento of arrInputs) {
@@ -14,31 +18,30 @@ for (var elemento of arrInputs) {
 document.addEventListener("click", function (event) {
   if (!arrInputs.includes(event.target)) {
     for (let i = 0; i < arrInputs.length; i++) {
-        let campos = arrInputs[i];
-        if (campos.classList.contains("focado")) {
-            campos.classList.remove("focado")
-        }
+      let campos = arrInputs[i];
+      if (campos.classList.contains("focado")) {
+        campos.classList.remove("focado");
+      }
     }
   }
   for (i of arrInputs) {
     if (i.classList.contains("focado")) {
-      i.style.height = "35px";
-    } else if (!i.classList.contains("focado") && i.value.length > 0) {
-      i.style.height = "35px";
+      label.style.transform = "translateY(-42px)"
     } else {
-      i.style.height = "";
+     
     }
   }
 });
 
-document.addEventListener("keydown", function () {
-  for (i of arrInputs) {
-    if (i.classList.contains("focado")) {
-      i.style.height = "35px";
-    } else if (!i.classList.contains("focado") && i.value.length > 0) {
-      i.style.height = "35px";
-    } else {
-      i.style.height = "";
-    }
-  }
-});
+
+// document.addEventListener("keydown", function () {
+//   for (i of arrInputs) {
+//     if (i.classList.contains("focado")) {
+//       i.style.height = "35px";
+//     } else if (!i.classList.contains("focado") && i.value.length > 0) {
+//       i.style.height = "35px";
+//     } else {
+//       i.style.height = "";
+//     }
+//   }
+// });
