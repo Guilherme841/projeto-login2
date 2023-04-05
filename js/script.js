741a97df6b340be69c3805bc0182b162a70e4142
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-04-04 22:15:05 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,41 +1,22 @@
 const inputs = document.querySelectorAll(".inputs");
 const arrInputs = Array.from(inputs);
-const input = document.getElementById("primeiroNome")
-const label = document.getElementsByTagName("label")
-const arrLabel = Array.from(label)
+const input = document.getElementById("primeiroNome");
+const label = document.getElementsByTagName("label");
+const arrLabel = Array.from(label);
 const label2 = document.getElementById("nome2");
-// const styleLabel = window.getComputedStyle(label);
 
 for (let e of arrInputs) {
-  e.addEventListener("focus",() => {
-    label2.style.transform = "translateY(-42px)"
-  })
-  e.addEventListener("blur", () => {
-    if (!input.value) {
-      label2.style.transform = "translateY(-25px)"
+  e.addEventListener("focus", function() {
+    inputClicado = this;
+    labelClicado = document.querySelector(`label[for="${inputClicado.id}"]`);
+    labelClicado.style.transform = "translateY(-42px)";
+  });
+  e.addEventListener("blur", function() {
+    if (!inputClicado.value) {
+      labelClicado.style.transform = "translateY(-25px)";
     }
-  })
+  });
 }
-
-// for (var elemento of arrInputs) {
-//   elemento.addEventListener("click", function () {
-//     for (i of arrInputs) {
-//       i.classList.remove("focado");
-//     }
-//     this.classList.add("focado");
-//   });
-// }
-
-// document.addEventListener("click", function (event) {
-//   if (!arrInputs.includes(event.target)) {
-//     for (let i = 0; i < arrInputs.length; i++) {
-//       let campos = arrInputs[i];
-//       if (campos.classList.contains("focado")) {
-//         campos.classList.remove("focado");
-//       }
-//     }
-//   }
-// });
 
 // document.addEventListener("keydown", function () {
 //   for (i of arrInputs) {
