f7127c0b95b4cbfbf2bbf69e8fe51a019db2724f
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-04-14 19:38:51 -0300

    update

--- modules/express.js
+++ modules/express.js
@@ -1,29 +1,3 @@
-// const express = require("express");
-// const UserModel = require("../src/models/user.model");
-
-// const app = express();
-// app.use(express.json());
-
-// // app.get("/nome", (req,res) => {
-// //     res.status(200).send("<h1>Hello world<h1>");
-// // })
-
-// app.post("/users", async (req, res) => {
-//   try {
-//     const user = await UserModel.create(req.body);
-
-//     res.status(201).json(user);
-//   } catch (error) {
-//     res.status(500).send(error.message);
-//   }
-// });
-
-// const port = 5500;
-
-// app.listen(port, () => {
-//   console.log(`Rodando com express na porta ${port}!`);
-// });
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const UserModel = require("../src/models/user.model");
