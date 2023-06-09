47e24f15340dc2384797e6755127939b5ed2c673
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-06-09 15:04:53 -0300

    update

--- modules/express.js
+++ modules/express.js
@@ -1,33 +1,75 @@
 const jwt = require("jsonwebtoken");
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const path = require("path")
+const cookieParser = require("cookie-parser")
 const UserModel = require("../src/models/user.model");
 require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname)));
+app.use(cookieParser());
 
-app.get("/cadastrado", async (req, res) => {
-  res.send("logado com sucesso!")
+// app.get("/cadastrado", async (req, res) => {
+//   res.send("Logado com sucesso!")
+// });
+
+app.use((req, res, next) => {
+  const token = req.cookies.token; // Obtém o token do cookie
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        console.log("token Invalido")
+        next();
+      } else {
+        // Token válido, definir informações de autenticação no objeto de solicitação (req)
+        console.log("token valido")
+        req.user = decoded;
+        res.sendFile(path.join(__dirname, "../logado.html"));
+      }
+    });
+  } else {
+    // Nenhum token encontrado, continuar sem autenticação
+    console.log("Nenhum token encontrado!")
+    next();
+  }
 });
 
 app.post("/users", async (req, res) => {
   try {
+
+    const { pass } = req.body;
+
+    const saltRound = 10
+    const hashedPass = await bcrypt.hash( pass , saltRound ) 
+
     const user = await UserModel.create({
       nome: req.body.nome,
       ultimoNome: req.body.ultimoNome,
       email: req.body.email,
-      pass: req.body.pass,
+      pass: hashedPass,
       profissao: req.body.profissao,
       data: req.body.data,
       telefone: req.body.telefone,
-      iestado: req.body.iestado,a
+      iestado: req.body.iestado,
     });
 
-    res.status(201).json(user);
-    console.log(req.body);
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    if (token) {
+      res.cookie('token', token, { secure: true, httpOnly: true });
+      res.sendFile(path.join(__dirname, "../logado.html"));
+    } else {
+      res.status(500).send("Erro ao criar token de autenticação");
+    }
+    // res.send("Cadastrado com sucesso!")
+    // res.status(201).json(user);
+    // console.log(req.body);
   } catch (error) {
     res.status(500).send(error.message);
+    console.log(error)
   }
 });
 
@@ -41,12 +83,11 @@ app.post("/login", async (req, res) => {
       return res.status(401).send("Email ou senha inválidos");
     }
 
-    if (user.pass !== pass) {
+    const sincPass = await bcrypt.compare(pass, user.pass);
+
+    if (!sincPass) {
       return res.status(401).send("Email ou senha inválidos");
     }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    // res.status(200).json({ token });
-    res.redirect("http://localhost:5500/cadastrado")
   } catch (error) {
     res.status(500).send(error.message);
   }
